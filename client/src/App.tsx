60b3c9162fed7f11a9f71909947ec4e914e983d8
NebulaTris <shambhavi.mishra2@s.amity.edu>
2023-08-31 02:04:03 +0530

    feat : files added/changed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,30 @@
-import { useMemo } from 'react';
-import { createTheme } from '@mui/material/styles';
-import { themeSettings } from './theme';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
-import { Box } from '@mui/material/';
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
+import Predictions from "@/scenes/predictions";
 
-function App () {
-  const theme = useMemo(() => createTheme(themeSettings), [])
-  return (<div className='app'>
-    <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box width="100%" height="100%" padding="1rem 2 rem 4rem 2rem">
-      <Routes>
-        <Route path="/" element={<div>Dashboard Page</div>} />
-        <Route path="/predictions" element={<div>Predictions</div>} />
-      </Routes>
-      </Box>
-    </ThemeProvider>
-    </BrowserRouter>
-  </div>
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   );
 }
 
