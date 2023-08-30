ce43897c21dc710ded04f473a2e71dee03eb0474
NebulaTris <shambhavi.mishra2@s.amity.edu>
2023-08-30 18:17:58 +0530

    feat : files added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,28 @@
+import { useMemo } from 'react';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { Box } from '@mui/material/';
 
 function App () {
-  return <div className='app'></div>
+  const theme = useMemo(() => createTheme(themeSettings), [])
+  return (<div className='app'>
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box width="100%" height="100%" padding="1rem 2 rem 4rem 2rem">
+      <Routes>
+        <Route path="/" element={<div>Dashboard Page</div>} />
+        <Route path="/predictions" element={<div>Predictions</div>} />
+      </Routes>
+      </Box>
+    </ThemeProvider>
+    </BrowserRouter>
+  </div>
+  );
 }
 
 export default App;
+
 
